79ff8bca36a2f26b954e69ea51a1ef1e5cc2ad3f
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2022-02-09 15:51:28 +0530

    plz_ merge master

--- src/Pack_selenium/RunInChrome.java
+++ src/Pack_selenium/RunInChrome.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class RunInChrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stud
 
 		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
