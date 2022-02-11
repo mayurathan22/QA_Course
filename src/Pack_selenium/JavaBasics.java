7d6db5eec0a50438c379c97aa4ffe9974f67edf2
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2022-02-11 16:10:08 +0530

    java practice

---
+++ src/Pack_selenium/JavaBasics.java
@@ -0,0 +1,13 @@
+package Pack_selenium;
+
+public class JavaBasics {
+	public static void main(String[] args) {
+		int[] arr = {4,5,6,8,4};
+//		System.out.println(arr[2]);
+		System.out.println(arr.length);
+		
+		for(int i =0; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
