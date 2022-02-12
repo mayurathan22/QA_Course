c077b382d38ab3ed715d2ab5b381196599dea16b
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2022-02-12 23:08:41 +0530

    array

---
+++ src/Pack_selenium/ArrayString.java
@@ -0,0 +1,12 @@
+package Pack_selenium;
+
+public class ArrayString {
+	public static void main(String[] args) {
+	
+	String [] ipl= {"csk","mi","rcb"};
+	
+	for(String s:ipl)
+		System.out.println(s);
+	}
+
+}
