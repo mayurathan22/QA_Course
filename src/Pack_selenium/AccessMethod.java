d2642ebb1b0fa13fa1bab96dfa9c1958d0b5bd9a
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2022-02-13 23:24:12 +0530

    methods_java

---
+++ src/Pack_selenium/AccessMethod.java
@@ -0,0 +1,16 @@
+package Pack_selenium;
+
+public class AccessMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AccessMethod a= new AccessMethod();
+		a.PrintData();
+	}
+	
+	public void PrintData() {
+		System.out.println("this is is Print data method");
+	}
+
+}
